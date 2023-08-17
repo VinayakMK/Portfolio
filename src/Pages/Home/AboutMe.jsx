d4d2d0e851924110482b38213587f7254a34994b
Vinayak MK <48713290+VinayakMK@users.noreply.github.com>
2023-08-18 01:23:39 +0530

    Add files via upload

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+    return (
+      <section id="AboutMe" className="about--section">
+        <div className="about--section--img">
+          <img src="./img/about-me.png" alt="About Me" />
+        </div>
+        <div className="hero--section--content--box about--section--box">
+          <div className="hero--section--content">
+            <p className="section--title">About</p>
+            <h1 className="skills-section--heading">About Me</h1>
+            <p className="hero--section-description">
+            So basically I am new to this field. As a 22 year old computer engineering student, 
+            who graduated in the year of 2022, I am looking to work as a beginner level UI/UX designer
+            in any company, because this is the field I mostly like. So I started learning about 
+            UI/UX designing like a year ago. I am still on way to learn full stack develoment.
+            </p>
+            <p className="hero--section-description">
+            Other than this, I game a lot and also a big fan of sports especially football. 
+            I wish I could travel alot.
+            So yeah that's me...
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
